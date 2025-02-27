f3b26d8494aee8ec58aa02a5618918dfdfce7e32
yulinyu <2508000341@QQ.COM>
2025-02-27 14:59:33 +0800

    1

---
+++ src/main/webapp/admin/src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+                return {
+            url : "http://localhost:8080/huamingb2cdianzhishangwu/",
+            name: "huamingb2cdianzhishangwu",
+            // 退出到首页链接
+            indexUrl: 'http://localhost:8080/huamingb2cdianzhishangwu/front/index.html'
+        };
+            },
+    getProjectName(){
+        return {
+            projectName: "“花鸣”B2C电子商务平台"
+        } 
+    }
+}
+export default base
